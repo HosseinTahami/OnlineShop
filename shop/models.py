a85ccef2ac4aa179d9ee7aea6defe5b20a415e71
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-15 15:40:15 +0330

    feat: Category model added

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,9 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     img = models.ImageField()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    img = models.ImageField()
