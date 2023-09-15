fc2675088ba911db56c48e4f43e56a9c430e54f1
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-15 15:21:24 +0330

    feat: user model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+# Django Imports
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    created_at = models.DateField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.id} || {self.username} || {self.email}"
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
