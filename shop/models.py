71564c01d3c3083f948bfaeb41680c7df7f90501
HosseinTahami <hosseintahami2000@gmail.com>
2023-09-15 15:37:49 +0330

    feat: Product created

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    price = models.DecimalField()
+    discount = models.DecimalField()
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    img = models.ImageField()
